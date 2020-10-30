9716e8f5602ba7485aa1fcbc40c78f2ad311752a
Darío González Martínez <dario@tercap.es>
2020-10-30 20:20:46 +0100

    Initial Commit

---
+++ AppleONE/AppleONEApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleONEApp.swift
+//  AppleONE
+//
+//  Created by Darío González Martínez on 30/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct AppleONEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppleONE/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AppleONE
+//
+//  Created by Darío González Martínez on 30/10/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
